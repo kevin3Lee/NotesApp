0b65dc35f7a51e00a41ff6c0886da0ec9ddd9a22
Max <maxnelson997@gmail.com>
2019-03-17 14:57:04 -0600

    added search/filter functionality

--- Notes/Controllers/FolderNotesController.swift
+++ Notes/Controllers/FolderNotesController.swift
@@ -15,19 +15,34 @@ class FolderNotesController: UITableViewController {
         didSet {
             self.navigationItem.title = noteFolder.title
             self.notes = noteFolder.notes
+            self.filteredNotes = noteFolder.notes
         }
     }
     
     var notes:[Note] = []
+    var filteredNotes:[Note] = []
+    fileprivate var cachedText:String = ""
     
     fileprivate let CELL_ID = "CELL_ID"
+    
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        self.navigationItem.title = "Folder Notes"
         setupTableView()
+        setupSearchBar()
     }
+    
+    fileprivate func setupSearchBar() {
+        self.definesPresentationContext = true
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.dimsBackgroundDuringPresentation = true
+        searchController.searchBar.delegate = self
+    }
+    
     
     fileprivate func setupTableView() {
         tableView.register(NoteCell.self, forCellReuseIdentifier: CELL_ID)
@@ -50,6 +65,36 @@ class FolderNotesController: UITableViewController {
     }
 }
 
+extension FolderNotesController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredNotes = notes.filter({ (note) -> Bool in
+            return note.title.lowercased().contains(searchText.lowercased())
+        })
+//        searchText.isEmpty && filteredNotes.isEmpty ? filteredNotes = notes : ()
+        if searchBar.text!.isEmpty && filteredNotes.isEmpty {
+            filteredNotes = notes
+        }
+        cachedText = searchText
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+//        searchBar.text!.isEmpty && filteredNotes.isEmpty ? filteredNotes = notes : ()
+
+        if !cachedText.isEmpty && !filteredNotes.isEmpty {
+            searchController.searchBar.text = cachedText
+        }
+        if searchBar.text!.isEmpty && filteredNotes.isEmpty {
+            filteredNotes = notes
+        }
+        tableView.reloadData()
+    }
+    
+    
+
+    
+}
+
 
 
 // MARK:- TABLEVIEW DELEGATE & DATA SOURCE METHODS
@@ -57,7 +102,7 @@ class FolderNotesController: UITableViewController {
 extension FolderNotesController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath) as! NoteCell
-        let note = notes[indexPath.row]
+        let note = filteredNotes[indexPath.row]
         cell.note = note
         return cell
     }
@@ -69,7 +114,7 @@ extension FolderNotesController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notes.count
+        return filteredNotes.count
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
